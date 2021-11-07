69798ee42241a9b535e02e35fab816c4f44ca146
tobi aroninuola <aroninuola.oluwatobi@gmail.com>
2021-11-07 14:26:24 +0100

    Completed Week 6 Exercise

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -73,3 +73,4 @@ class DataModel {
 // Do not worry about the below for now; It is included so that we can test your code
 // We will cover module exports in later parts of this course
 module.exports = DataModel;
+
